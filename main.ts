18b2da9882fca2acaa0a8254bbc99a47586c5d9a
Abdullah Babar <164386517+Abdullah-Babar@users.noreply.github.com>
2024-09-01 23:00:13 +0500

    number guessing game

---
+++ main.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer"
+
+// Generate random number
+
+const randomNumber = Math.floor(Math.random() * 6 + 1 )
+
+// asking user 
+
+const number = await inquirer.prompt({
+    name: "GuessedNumber",
+    type: "number",
+    message: "Plese enter the number between 1-6"
+})
+
+// condition 
+
+if (number.GuessedNumber === randomNumber){
+    console.log("Congratulations! you gussed correct answer");
+}
+
+else {
+    console.log(`You guessed wrong answer. The correct answer is ${randomNumber}`);
+}
